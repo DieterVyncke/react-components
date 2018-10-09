3723904f25e57a3ae647a9cf427ce452ad5a2541
Rein Van Oyen <reinvanoyen@gmail.com>
2018-10-09 14:54:27 +0200

    carousel component added

---
+++ src/carousel/index.js
@@ -0,0 +1,104 @@
+"use strict";
+
+import React from 'react';
+
+export default class Carousel extends React.Component {
+
+  static defaultProps = {
+    align: 'center',
+    onGoTo: (i) => {}
+  };
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      activeIndex: 0,
+      translateX: 0
+    };
+  }
+
+  goTo(index, emitEvent = true) {
+
+    this.setState({
+      activeIndex: index
+    });
+
+    if (emitEvent) {
+      this.props.onGoTo(index);
+    }
+  }
+
+  goToPrev() {
+
+    let prevIndex = this.state.activeIndex-1;
+
+    if (prevIndex <= 0) {
+      this.goTo(this.props.children.length-1);
+    } else {
+      this.goTo(prevIndex);
+    }
+  }
+
+  goToNext() {
+
+    let nextIndex = this.state.activeIndex+1;
+
+    if (nextIndex >= this.props.children.length) {
+      this.goTo(0);
+    } else {
+      this.goTo(nextIndex);
+    }
+  }
+
+  calibrateTransforms() {
+
+    if (this.refs['slide-'+this.state.activeIndex]) {
+
+      let wrapper = this.refs['wrapper'];
+      let activeSlide = this.refs['slide-'+this.state.activeIndex];
+      let offsetX = 0;
+
+      switch(this.props.align) {
+        case 'center':
+          offsetX = -activeSlide.offsetLeft + (wrapper.offsetWidth/2) - (activeSlide.offsetWidth/2);
+          break;
+        case 'left':
+          offsetX = -activeSlide.offsetLeft;
+          break;
+        case 'right':
+          offsetX = -activeSlide.offsetLeft + wrapper.offsetWidth - activeSlide.offsetWidth;
+          break;
+      }
+
+      this.setState({
+        translateX: offsetX
+      });
+    }
+  }
+
+  componentDidUpdate(oldProps, oldState) {
+    if (oldState.activeIndex !== this.state.activeIndex) {
+      this.calibrateTransforms();
+    }
+  }
+
+  componentDidMount() {
+    this.calibrateTransforms();
+  }
+
+  render() {
+    return (
+      <div className="carousel" ref="wrapper">
+        <div className="carousel__wrap" style={{transform: 'translateX('+this.state.translateX+'px)'}}>
+          {this.props.children.map((child, i) => (
+            <div key={i} className={'carousel__slide' + (this.state.activeIndex === i ? ' carousel__slide--active' : '')} ref={'slide-'+i} onClick={e => this.goTo(i)}>
+              {child}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import ConfirmOverlay from '../src/confirm-overlay/index';
 import DotPagination from '../src/dot-pagination/index';
 import Overlay from '../src/overlay/index';
 import LoadingImage from '../src/loading-image/index';
+import Carousel from '../src/carousel/index';
 
 storiesOf('Button', module)
   .addDecorator(withKnobs)
@@ -65,4 +66,28 @@ storiesOf('DotPagination', module)
   .add('Basic', () => (
     <DotPagination amount={number('amount', 5)} />
   ))
+;
+
+storiesOf('Carousel', module)
+  .addDecorator(withKnobs)
+  .add('Basic', () => {
+
+    let style = (width, color) => {
+      return {
+        backgroundColor: color,
+        width: width+'px',
+        height: '350px'
+      };
+    };
+
+    return (
+      <Carousel align={text('align', 'center')}>
+        <div style={style(600, 'red')}>Nice #1</div>
+        <div style={style(250, 'yellow')}>Nice #2</div>
+        <div style={style(300, 'blue')}>Nice #3</div>
+        <div style={style(400, 'purple')}>Nice #4</div>
+        <div style={style(750, 'orange')}>Nice #5</div>
+      </Carousel>
+    );
+  })
 ;
